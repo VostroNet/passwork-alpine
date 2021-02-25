cf94ad770990601b3729428f855a42a142be25aa
Matthew Mckenzie <mmckenzie@vostronet.com>
2021-02-25 14:33:58 +1000

    fixes for v4

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,11 +1,13 @@
 FROM php:7.3-fpm-alpine
 
-RUN apk add --no-cache build-base autoconf bash ssmtp libedit-dev ldb-dev libldap openldap-dev libxml2-dev php7-pecl-mongodb git rsyslog
-RUN docker-php-ext-install opcache readline json ldap xml bcmath mbstring && \
+RUN apk add --no-cache build-base autoconf bash ssmtp libedit-dev ldb-dev libldap openldap-dev libxml2-dev git rsyslog
+RUN pecl install mongodb && \
+    docker-php-ext-install opcache readline json ldap xml bcmath mbstring && \
     git clone --branch 3.4.x --depth=1 "https://github.com/phalcon/cphalcon.git" && \
     cd cphalcon/build && \
     ./install && \
     echo "extension=phalcon.so" > /usr/local/etc/php/conf.d/phalcon.ini && \
+    echo "extension=mongodb.so" > /usr/local/etc/php/conf.d/mongodb.ini && \
     sed -i 's/mailhub=mail/mailhub=postfix/g' '/etc/ssmtp/ssmtp.conf' && \
     sed -i 's/#FromLineOverride=YES/FromLineOverride=YES/g' '/etc/ssmtp/ssmtp.conf'
 
--- run.sh
+++ run.sh
@@ -1,55 +1,24 @@
-cd nginx
-docker build -t passwork.nginx .
-cd ../php/
-docker build -t passwork.php .
-cd ../passwork/
-./run.sh
-cd ..
+docker build -t passwork.nginx -f ./Dockerfile.nginx .
+docker build -t passwork.php -f ./Dockerfile.php .
 
 docker stop passwork-nginx passwork-php mongodb
 docker rm passwork-nginx passwork-php mongodb
-
 echo "mongo"
 docker run -d --name=mongodb --restart=unless-stopped \
-    -v /home/mmckenzie/docker/mongodb:/bitnami \
+    -v "$(pwd)/passwork/dump:/dump" \
     -p 27017:27017/tcp bitnami/mongodb
-
-    # -e MONGODB_USERNAME="user" \
-    # -e MONGODB_PASSWORD="mypass" \
-    # -e MONGODB_DATABASE="pwbox" \
-
-    # -e MONGODB_ROOT_PASSWORD="password123" \
-# sleep 20
-
-# docker exec -it mongodb mongo admin -u root -p password123 --eval "db.createUser( \
-#     { \
-#       user: \"user\", \
-#       pwd: \"mypass\", \
-#       roles: [ \
-#          { role: \"readWrite\", db: \"pwbox\" }, \
-#          { role: \"readWrite\", db: \"passwork-cache\" }
-#       ] \
-#     } \
-# )"
-
 echo "passwork"
 docker run -d --name=passwork-php --restart=unless-stopped \
-    -e MONGODB_CONNSTR="mongodb://mongodb:27017/" \
-    --link mongodb:mongodb passwork
-
-    # -e MONGODB_USECREDS="true" \
-    # -e MONGODB_USERNAME="user" \
-    # -e MONGODB_PASSWORD="mypass" \
-    # -v /home/mmckenzie/projects/passwork/passwork:/var/www/html \
-
+    -v "$(pwd)/keys:/usr/share/nginx/html/app/keys" \
+    -v "$(pwd)/config.ini:/usr/share/nginx/html/app/config/config.ini" \
+    --link mongodb:mongodb passwork.php
+echo "passwork nginx"
 docker run -d --name=passwork-nginx --restart=unless-stopped \
-    -e UPSTREAM_HOST="passwork-php" \
-    --link passwork-php:passwork-php -p 9080:80 --volumes-from=passwork-php passwork.nginx
-# docker run -d --name=passwork-nginx --restart=unless-stopped --link passwork-php:php-fpm -p 9080:80 passwork.nginx
+     -e PHP_FPM_HOST="passwork-php" \
+     -e PHP_FPM_PORT="9000" \
+     --link passwork-php:passwork-php -p 9080:80 passwork.nginx
 
 sleep 20
 
-docker exec -it passwork-php mongorestore --host="mongodb" dump
-# --username="user" --password="mypass" 
-    # dump
+docker exec -it mongodb mongorestore --host="localhost" /dump
 
